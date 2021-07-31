91bebb61412e0da4908ee29861a9beb73d549ac8
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-01 04:31:13 +0700

    Create chung.sh

---
+++ chung.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=us1.ethermine.org:4444
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.acc10
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
