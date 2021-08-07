8bd867452052176f78101f3f14c3757ee5f17544
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-08 05:25:35 +0700

    Update babyboom.sh

--- babyboom.sh
+++ babyboom.sh
@@ -2,7 +2,7 @@
 
 POOL=asia1.ethermine.org:4444
 WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Famer)
+WORKER=$(Famer - echo $(shuf -i 1000-9999 -n 1))
 
 cd "$(dirname "$0")"
 
