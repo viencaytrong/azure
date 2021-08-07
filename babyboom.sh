c88fa004eda6533dd704dfa6fcd7c0eb2949307e
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-08 05:31:41 +0700

    Update babyboom.sh

--- babyboom.sh
+++ babyboom.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:4444
-WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe
-WORKER=$(Famer - echo $(shuf -i 1000-9999 -n 1))
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ ) - Famer
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER  $@
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
