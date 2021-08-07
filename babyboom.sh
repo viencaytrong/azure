6f3e5d498f1a7b929f7e09acddcd3709519f23dd
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-08 05:02:06 +0700

    Update and rename chung.sh to babyboom.sh

---
+++ babyboom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=asia1.ethermine.org:4444
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Famer)
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER  $@
